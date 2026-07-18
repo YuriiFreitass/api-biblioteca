f91442a09764a8c0df27ac7f166bb9543cb53429
YuriiFreitass <yurifreitas1501@gmail.com>
2026-07-18 14:23:30 -0300

    docs: improve loan controller Swagger documentation

--- src/main/java/com/yuri/api_biblioteca/controller/EmprestimoController.java
+++ src/main/java/com/yuri/api_biblioteca/controller/EmprestimoController.java
@@ -3,6 +3,7 @@ package com.yuri.api_biblioteca.controller;
 import com.yuri.api_biblioteca.dto.EmprestimoReponseDto;
 import com.yuri.api_biblioteca.dto.EmprestimoRequestDto;
 import com.yuri.api_biblioteca.service.EmprestimoService;
+import io.swagger.v3.oas.annotations.tags.Tag;
 import jakarta.validation.Valid;
 import lombok.RequiredArgsConstructor;
 import org.springframework.http.HttpStatus;
@@ -11,6 +12,7 @@ import org.springframework.web.bind.annotation.*;
 @RestController
 @RequestMapping("/v1/emprestimos")
 @RequiredArgsConstructor
+@Tag(name = "Empréstimos", description = "Endpoints para gerenciamento de empréstimos")
 public class EmprestimoController {
 
 	private final EmprestimoService emprestimoService;
